4287c86e5dbf037ea811a4a3d1a75f1ff0e229a3
wellandrade <leto.l@hotmail.com>
2025-01-28 12:21:08 -0300

    feat: Adicionada a ultima função do desafio.(Desafio concluido)

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
-function adicionarAmigo() {
+const amigos = [];
+
+function adicionarAmigo(lista) {
   const campo = document.querySelector("#amigo");
   let addNome = document.getElementById("amigo").value;
   if (addNome != "") {
+    amigos.push(addNome);
     let lista = document.getElementById("listaAmigos");
     let novoItem = document.createElement("li");
     novoItem.textContent = addNome;
@@ -11,4 +14,18 @@ function adicionarAmigo() {
     alert("Entrada invalida, escreva algo no campo!");
   }
   campo.value = "";
+}
+
+function sortearAmigo() {
+  const listaLimpar = document.getElementById("listaAmigos");
+  listaLimpar.innerHTML = "";
+  if (amigos.length === 0) {
+    alert("Adicione amigos antes de sortear!");
+  }
+
+  const nomeSorteado = Math.floor(Math.random() * amigos.length);
+  const amigoSorteado = amigos[nomeSorteado];
+
+  let resultado = document.getElementById("resultado");
+  resultado.textContent = `amigo sorteado é ${amigoSorteado}`;
 }
