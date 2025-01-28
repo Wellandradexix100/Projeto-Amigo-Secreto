04047c59d80b84589bd0b5bd0fa479f694a5b1aa
wellandrade <leto.l@hotmail.com>
2025-01-28 11:57:17 -0300

    feat: Criação da lista funcionando.

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+function adicionarAmigo() {
+  const campo = document.querySelector("#amigo");
+  let addNome = document.getElementById("amigo").value;
+  if (addNome != "") {
+    let lista = document.getElementById("listaAmigos");
+    let novoItem = document.createElement("li");
+    novoItem.textContent = addNome;
+    lista.appendChild(novoItem);
+  } else {
+    alert("Entrada invalida, escreva algo no campo!");
+  }
+  campo.value = "";
+}
